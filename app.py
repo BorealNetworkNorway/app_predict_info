30069cf5440e01d8f54be446604d478915f861d0
BorealNetworkNorway <borealnetwork.norway@gmail.com>
2025-05-26 14:18:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ for plot_id, df in data_by_plot.items():
             [lat, lon],
             icon=folium.DivIcon(
                 html=f"""
-                    <div style="font-size: 10px;font-weight: bold; color: white; background-color: purple; border-radius: 4px; padding: 2px 4px; text-align: center;  transform: translate(-50%, -50%); position: relative; width: auto;">
+                    <div style="font-size: 10px;font-weight: bold; color: white; background-color: purple; border-radius: 4px; padding: 3px 4px; text-align: left; position: relative; width: auto;">
                         {plot_id}
                     </div>
                 """
