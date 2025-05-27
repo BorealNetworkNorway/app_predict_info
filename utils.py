4d5664d882086ac9242c7ffe93103b6058fa0969
BorealNetworkNorway <borealnetwork.norway@gmail.com>
2025-05-27 07:09:54 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -66,9 +66,10 @@ def show_tree_map(df, show_dendrometers=False, show_labels=False):
     species_layer = base.mark_circle().encode(
         size=alt.Size("mean_dbh", scale=alt.Scale(range=[30, 200])),
         color=alt.condition(
-            show_dendrometers,
-            alt.condition("datum.has_dendrometer", alt.value("orange"), alt.Color("species:N")),
-            alt.Color("species:N")
+                show_dendrometers,
+                alt.condition("datum.has_dendrometer", alt.value("orange"), alt.Color("species:N")),
+                alt.Color("species:N")
+        )
     )
 
     
